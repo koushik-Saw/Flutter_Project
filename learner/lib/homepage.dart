4d1b6182ff2a64cf029d1fb798f379baca36a673
koushik-Saw <koushik15-7242@diu.edu.bd>
2021-08-19 08:09:55 +0600

    Homepage Updated

--- learner/lib/homepage.dart
+++ learner/lib/homepage.dart
@@ -1,10 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,9 +18,15 @@ class Home extends StatelessWidget {
   }
 }
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
 
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+late List Data;
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
@@ -24,23 +34,23 @@ class Homepage extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.black,
         body: SingleChildScrollView(
-      child: Column(
-        children: <Widget>[
-          Searchbar(size: size),
-          Container(
-            child: Positioned(
-                child: Container(
-              color: Colors.black,
-              height: size.height * .7,
-              child: ListView(),
-            )),
+          child: Column(
+            children: <Widget>[
+              Searchbar(size: size),
+              Container(
+                child: Positioned(
+                    child: Container(
+                  color: Colors.black,
+                  height: size.height * .7,
+                  child: ListView(),
+                )),
+              ),
+              Bottombar(
+                size: size,
+              )
+            ],
           ),
-          Bottombar(
-            size: size,
-          )
-        ],
-      ),
-    ));
+        ));
   }
 }
 
@@ -160,44 +170,3 @@ class Searchbar extends StatelessWidget {
     );
   }
 }
-
-// class Homepage extends StatefulWidget {
-//   const Homepage({Key? key}) : super(key: key);
-//
-//   @override
-//   _HomeState createState() => _HomeState();
-// }
-//
-// Icon cusicon = Icon(Icons.notifications);
-// Widget custitle = Text("Home");
-//
-// class _HomeState extends State<Homepage> {
-//   @override
-//   Widget build(BuildContext context) {
-//
-//
-//     // return Scaffold(
-//     //   // appBar: AppBar(
-//     //   //   title: custitle,
-//     //   //   leading: Icon(Icons.home_outlined),
-//     //   //   actions: [
-//     //   //     IconButton(
-//     //   //       onPressed: () {},
-//     //   //       icon: cusicon,
-//     //   //     ),
-//     //   //     IconButton(onPressed: () {}, icon: Icon(Icons.person))
-//     //   //     // IconButton(
-//     //   //     //     onPressed: () {
-//     //   //     //       FirebaseAuth.instance.signOut();
-//     //   //     //       Navigator.push(
-//     //   //     //           context, MaterialPageRoute(builder: (context) => login()));
-//     //   //     //     },
-//     //   //     //     icon: Icon(Icons.logout))
-//     //   //   ],
-//     //   // ),
-//     //   body: getdata(),
-//     // );
-//   }
-//
-//   getdata() {}
-// }
