02565016ceabf033d3ef0f48458e9368a51d74d4
koushik-Saw <koushik15-7242@diu.edu.bd>
2021-11-28 16:30:17 +0600

    profile manage

--- food_door/lib/pages/manageprofile.dart
+++ food_door/lib/pages/manageprofile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:food_door/model/listtile.dart';
 
 class Manageprofile extends StatefulWidget {
   const Manageprofile({Key? key}) : super(key: key);
@@ -11,8 +12,33 @@ class Manageprofile extends StatefulWidget {
 class _ManageprofileState extends State<Manageprofile> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
       backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
+                ),
+              ),
+            ),
+            listitle(icn: Icon(Icons.person), listtitle: "Name", wiz: Icon(Icons.edit_outlined,color: Color(0xFF0D1266))),
+            listitle(icn: Icon(Icons.lock), listtitle: "Password", wiz: Icon(Icons.chevron_right_outlined,color: Color(0xFF0D1266))),
+            listitle(icn: Icon(Icons.mail), listtitle: "Mail", wiz: Icon(Icons.chevron_right_outlined,color: Color(0xFF0D1266))),
+            listitle(icn: Icon(Icons.call), listtitle: "Phone", wiz: Icon(Icons.chevron_right_outlined,color: Color(0xFF0D1266))),
+            listitle(icn: Icon(Icons.location_on_outlined,), listtitle: "Address", wiz: Icon(Icons.chevron_right_outlined,color: Color(0xFF0D1266))),
+            listitle(icn: Icon(Icons.language_outlined), listtitle: "Language", wiz: Icon(Icons.chevron_right_outlined,color: Color(0xFF0D1266))),
+            listitle(icn: Icon(Icons.info_outlined), listtitle: "Gender", wiz: Icon(Icons.chevron_right_outlined,color: Color(0xFF0D1266))),
+          ],
+        ),
+      ),
     );
   }
 }
--- food_door/lib/pages/nearme.dart
+++ food_door/lib/pages/nearme.dart
@@ -12,8 +12,36 @@ class _NearmeState extends State<Nearme> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Near Me"),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            pinned: true,
+            snap: true,
+            title: Text("Sliver"),
+            backgroundColor: Colors.lightGreen,
+            expandedHeight: 200,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.network(
+                "https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                return Container(
+                  color: index.isOdd ? Colors.white : Colors.black12,
+                  height: 100.0,
+                  child: Center(
+                    child: Text('$index', textScaleFactor: 5),
+                  ),
+                );
+              },
+              childCount: 10,
+            ),
+          ),
+        ],
       ),
     );
   }
