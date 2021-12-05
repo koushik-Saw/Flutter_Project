22276d2b0b23dee41be49880c0d0ccd9f08c026b
Koushik Majumder <koushik15-7242@diu.edu.bd>
2021-12-05 10:01:04 +0600

    Home Updated

--- food_door/lib/model/searchbar.dart
+++ food_door/lib/model/searchbar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'bottomtextbutton.dart';
+
 class Searchbar extends StatefulWidget {
   const Searchbar({Key? key}) : super(key: key);
 
@@ -11,19 +13,35 @@ class Searchbar extends StatefulWidget {
 class _SearchbarState extends State<Searchbar> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50,
-      child: TextFormField(
-        decoration: InputDecoration(
-            prefixIcon: const Icon(Icons.search_outlined,color: Color(0xfff15a24),),
-            suffixIcon: const Icon(Icons.tune_outlined,color: Color(0xfff15a24),),
-            filled: true,
-            fillColor: const Color(0xffffffff),
-            hintText: "Search",
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(16))),
-        style: const TextStyle(fontSize: 16),
-      ),
+    return Column(
+      children: [
+        SizedBox(
+          height: 50,
+          child: TextFormField(
+            decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search_outlined,color: Color(0xfff15a24),),
+                suffixIcon: const Icon(Icons.tune_outlined,color: Color(0xfff15a24),),
+                filled: true,
+                fillColor: const Color(0xffffffff),
+                hintText: "Search",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16))),
+            style: const TextStyle(fontSize: 16),
+          ),
+
+        ),
+        const SizedBox(height: 20,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const [
+            Bottomtextbutton(icons: Icon(Icons.lunch_dining_outlined,color: Colors.white,), title: "Burger",clr: Colors.white,),
+            Bottomtextbutton(icons: Icon(Icons.local_pizza_outlined,color: Colors.white,), title: "Pizza",clr: Colors.white,),
+            Bottomtextbutton(icons: Icon(Icons.ramen_dining_outlined,color: Colors.white,), title: "Breakfast",clr: Colors.white,),
+            Bottomtextbutton(icons: Icon(Icons.coffee_outlined,color: Colors.white,), title: "Coffee",clr: Colors.white,),
+            Bottomtextbutton(icons: Icon(Icons.local_bar_outlined,color: Colors.white,), title: "Drink",clr: Colors.white,),
+          ],
+        )
+      ],
     );
   }
 }
--- food_door/lib/pages/homepage.dart
+++ food_door/lib/pages/homepage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:food_door/model/bottomtextbutton.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
 import 'package:food_door/model/searchbar.dart';
 
 import 'account.dart';
@@ -17,6 +20,7 @@ class _HomepageState extends State<Homepage> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
+      backgroundColor: const Color(0xffc2c2cb),
       appBar: AppBar(
         leading: Padding(
           padding: const EdgeInsets.all(10.0),
@@ -47,50 +51,122 @@ class _HomepageState extends State<Homepage> {
           )
         ],
       ),
-      body: Column(
-        children: [
-          ClipPath(
-            clipper: CustomShape(),
-            child: Container(
-              height: size.height * .28,
-              color: const Color(0xfff15a24),
-              child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    children: [const Searchbar(),
-                    const SizedBox(height: 20,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: const [
-                        Bottomtextbutton(icons: Icon(Icons.lunch_dining_outlined,color: Colors.white,), title: "Burger",clr: Colors.white,),
-                        Bottomtextbutton(icons: Icon(Icons.local_pizza_outlined,color: Colors.white,), title: "Pizza",clr: Colors.white,),
-                        Bottomtextbutton(icons: Icon(Icons.ramen_dining_outlined,color: Colors.white,), title: "Breakfast",clr: Colors.white,),
-                        Bottomtextbutton(icons: Icon(Icons.coffee_outlined,color: Colors.white,), title: "Coffee",clr: Colors.white,),
-                        Bottomtextbutton(icons: Icon(Icons.local_bar_outlined,color: Colors.white,), title: "Drink",clr: Colors.white,),
-                      ],
-                    )
-
-                    ],
-                  )),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-            color: Colors.black,
-              height: size.height * .53,
-              width: double.infinity,
-              child: SingleChildScrollView(
-                child:
-                Column(
-                  children: const [
-
-                  ],
-                ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ClipPath(
+              clipper: CustomShape(),
+              child: Container(
+                height: size.height * .28,
+                color: const Color(0xfff15a24),
+                child: const Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Searchbar(),),
               ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+              color: const Color(0xffc2c2cb),
+                height: size.height * .52,
+                width: double.infinity,
+                child: SingleChildScrollView(
+                  child:
+                  Column(
+                    children:  [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("Favourites"),
+                          Text("Clear All"),
+                        ],
+                      ),
+                      const SizedBox(height: 10,),
+                      Container(
+                        color: const Color(0xffc2c2cb),
+                        height: 200,
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: const [
+                              Favourites(),
+                              SizedBox(width: 10,),
+                              Favourites(),
+                              SizedBox(width: 10,),
+                              Favourites()
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10,),
+                      Container(
+                        color: Colors.grey,
+                        height: 200,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Favourites extends StatelessWidget {
+  const Favourites({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(15))),
+      height: 180,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(borderRadius: const BorderRadius.all(Radius.circular(15.0)),child: Image.network("https://images.unsplash.com/photo-1626229652216-e5bb7f511917?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80",width: 150,height: 100,fit: BoxFit.cover,)),
+            const SizedBox(height: 5,),
+            const Text("Burger King"),
+            const SizedBox(height: 5,),
+            Row(
+              children: const [
+                Text("Burger",style: TextStyle(fontSize: 10),),
+                SizedBox(width: 5,),
+                Text("American Food",style: TextStyle(fontSize: 10),),
+                SizedBox(width: 5,),
+                Text("Deshi Food",style: TextStyle(fontSize: 10),),
+                SizedBox(width: 5,),
+              ],
+            ),
+            Row(
+
+              children: [
+                Row(
+                  children: const [
+                    Text("*",style: TextStyle(fontSize: 10),),
+                    Text("Burger",style: TextStyle(fontSize: 10),),
+                    Text("Burger",style: TextStyle(fontSize: 8),),
+                  ],
+                ),
+                const SizedBox(width: 18,),
+                Row(
+                  children: const [
+                    Text("Burger",style: TextStyle(fontSize: 10),),
+                    Text("Burger",style: TextStyle(fontSize: 10),),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
